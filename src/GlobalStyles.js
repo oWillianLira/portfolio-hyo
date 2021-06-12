6c891eedf9b98360a18007dcd7416e017979c567
Willian Lira <will.lirag3@gmail.com>
2021-06-12 12:51:00 -0300

    homepage first things and global styles using styled comp

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -1,10 +1,15 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
 body {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
   font-family: Dubai;
   font-size: 16px;
+  letter-spacing: 1px;
   font-weight: normal;
+  outline: none;
 }
 
 /* width */
@@ -51,3 +56,26 @@ body {
 a {
   text-decoration: none;
 }
+
+img {
+  max-width: 100%;
+}
+
+h1 { font-size: 3.5em; }
+h2 { font-size: 3em; }
+h3 { font-size: 2.5em; }
+h4 { font-size: 2em; }
+
+span { font-size: .9em; }
+
+`;
+
+export const colors = {
+  blue1: '#5A86A9',
+  blue2: '#3A4D5C',
+  blue3: '#212032',
+  brown1: '#D1B9A1',
+  brown2: '#C45030',
+};
+
+export default GlobalStyle;
