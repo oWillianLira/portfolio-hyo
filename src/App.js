05a838f963bfae5111c76c3262f2e1c72e686a7a
Willian Lira <will.lirag3@gmail.com>
2021-06-19 10:54:44 -0300

    updations for  homepage - menu - transition

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './routes';
-import history from './Services/history.js';
 import GlobalStyle from './GlobalStyles';
-import './route-animation.css';
 
 function App() {
   return (
-    <Router history={history}>
+    <Router>
       <GlobalStyle />
       <Routes />
     </Router>
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, useLocation, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 import Home from './Views/Home';
@@ -7,17 +7,19 @@ import About from './Views/About';
 import Contact from './Views/Contact';
 
 export default function Routes() {
-  const location = useLocation();
-
   return (
-    <TransitionGroup>
-      <CSSTransition key={location.key} classNames="transition" timeout={500}>
-        <Switch location={location}>
-          <Route path="/" exact component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} />
-        </Switch>
-      </CSSTransition>
-    </TransitionGroup>
+    <Route
+      render={({ location }) => (
+        <TransitionGroup>
+          <CSSTransition key={location.key} classNames="fade" timeout={500}>
+            <Switch location={location}>
+              <Route path="/" exact component={Home} />
+              <Route path="/about/" component={About} />
+              <Route path="/contact/" component={Contact} />
+            </Switch>
+          </CSSTransition>
+        </TransitionGroup>
+      )}
+    ></Route>
   );
 }
