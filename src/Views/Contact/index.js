80e26fd49439e934789b0e9831957322768b39c7
Willian Lira <will.lirag3@gmail.com>
2021-06-19 11:14:48 -0300

    updating contact page

--- src/Views/Contact/index.js
+++ src/Views/Contact/index.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { colors } from '../../GlobalStyles';
-
 import Menu from '../Components/Menu';
 
+import telegram from '../../Assets/Images/telegram.png';
+import mail from '../../Assets/Images/email.png';
+import linkedin from '../../Assets/Images/linkedin.png';
+import girl from '../../Assets/Images/talk_girl.png';
+
 export default function Contact() {
-  const View = styled.main`
+  const View = styled.section`
     background: transparent radial-gradient(40% 40% at 25% 50%, #d69050 0%, #ac703a 100%) 0% 0% no-repeat padding-box;
     width: 100%;
     min-height: 100vh;
-    padding: 50px 0;
+    padding: 50px 0 5px;
     display: flex;
     flex-direction: column;
     .container {
-      flex: 1;
       display: flex;
       flex-direction: column;
-      .row {
-        flex: 1;
-      }
+      flex: 1;
     }
   `;
 
@@ -77,6 +77,9 @@ export default function Contact() {
       margin-top: 8px;
       transition: 1000ms ease-in-out;
     }
+    @media screen and (max-width: 1199px) {
+      /* margin-bottom: 50px; */
+    }
     @media screen and (max-width: 767px) {
       height: 130px;
       width: 130px;
@@ -87,21 +90,189 @@ export default function Contact() {
     }
   `;
 
+  const Circles = styled.div`
+    width: 100%;
+    text-align: center;
+    svg {
+      max-width: 30%;
+    }
+    @media screen and (max-width: 1199px) {
+      text-align: left;
+    }
+  `;
+
+  const ContactAddress = styled.address`
+    position: relative;
+    min-height: 100px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    a img {
+      width: 75px;
+      height: auto;
+    }
+    a span {
+      color: #fff;
+      text-transform: uppercase;
+      font-weight: 700;
+      font-size: 1.5em;
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      width: 100%;
+      transform: translate(-50%, -50%);
+      transition: 300ms ease-in;
+    }
+    &:hover span {
+      top: 100%;
+    }
+  `;
+
+  const Girl = styled.div`
+    flex: 1;
+    display: flex;
+    justify-content: center;
+    align-items: flex-end;
+    img {
+      width: 650px;
+      @media screen and (max-width: 991px) {
+        width: 350px;
+      }
+    }
+  `;
+
   return (
     <main className="page">
       <View>
         <div className="container">
           <div className="row">
-            <div className="col-">
-              <Resume href="#">
+            <div className="col-xxl-3 col-xl-4 col-md-5">
+              <Resume
+                target="_blank"
+                href="https://drive.google.com/file/d/128XDkeQmIZ5WHqU5ayQ3mUybW4NZdOJz/view?usp=sharing"
+              >
                 <span>
                   Get my <br />
                   resume
                 </span>
               </Resume>
             </div>
-            <Link to="/">HOME</Link>
+            <div className="col-xxl-6 col-xl-4 col-md-12">
+              <Circles>
+                <svg xmlns="http://www.w3.org/2000/svg" width="165" height="45" viewBox="0 0 165 45">
+                  <g id="circles3" transform="translate(-878 -75)">
+                    <g
+                      id="Elipse_6"
+                      data-name="Elipse 6"
+                      transform="translate(878 75)"
+                      fill="none"
+                      stroke="#3a4d5c"
+                      stroke-width="2"
+                    >
+                      <circle cx="15" cy="15" r="15" stroke="none" />
+                      <circle cx="15" cy="15" r="14" fill="none" />
+                    </g>
+                    <g
+                      id="Elipse_7"
+                      data-name="Elipse 7"
+                      transform="translate(923 75)"
+                      fill="none"
+                      stroke="#3a4d5c"
+                      stroke-width="2"
+                    >
+                      <circle cx="15" cy="15" r="15" stroke="none" />
+                      <circle cx="15" cy="15" r="14" fill="none" />
+                    </g>
+                    <g
+                      id="Elipse_8"
+                      data-name="Elipse 8"
+                      transform="translate(968 75)"
+                      fill="none"
+                      stroke="#3a4d5c"
+                      stroke-width="2"
+                    >
+                      <circle cx="15" cy="15" r="15" stroke="none" />
+                      <circle cx="15" cy="15" r="14" fill="none" />
+                    </g>
+                    <g
+                      id="Elipse_9"
+                      data-name="Elipse 9"
+                      transform="translate(1013 75)"
+                      fill="none"
+                      stroke="#3a4d5c"
+                      stroke-width="2"
+                    >
+                      <circle cx="15" cy="15" r="15" stroke="none" />
+                      <circle cx="15" cy="15" r="14" fill="none" />
+                    </g>
+                    <circle
+                      id="Elipse_10"
+                      data-name="Elipse 10"
+                      cx="15"
+                      cy="15"
+                      r="15"
+                      transform="translate(900 90)"
+                      fill="#d1b9a1"
+                    />
+                    <circle
+                      id="Elipse_11"
+                      data-name="Elipse 11"
+                      cx="15"
+                      cy="15"
+                      r="15"
+                      transform="translate(945 90)"
+                      fill="#d1b9a1"
+                    />
+                    <circle
+                      id="Elipse_12"
+                      data-name="Elipse 12"
+                      cx="15"
+                      cy="15"
+                      r="15"
+                      transform="translate(990 90)"
+                      fill="#d1b9a1"
+                    />
+                  </g>
+                </svg>
+              </Circles>
+            </div>
+            <div className="col-xxl-3 col-xl-4 col-md-12 text-end">
+              <Menu c1="#fff" c2={colors.brown2}></Menu>
+            </div>
+            <div className="col-lg-4 text-center">
+              <ContactAddress>
+                <a href="https://t.me/hyosz" title="Text me on Telegram" target="_blank" rel="noopener noreferrer">
+                  <img src={telegram} alt="Telegram Icon" />
+                  <span>t.me/hyosz</span>
+                </a>
+              </ContactAddress>
+            </div>
+            <div className="col-lg-4 text-center">
+              <ContactAddress>
+                <a href="mailto:hyolanda.h@gmail.com" title="Send me a email" target="_blank" rel="noopener noreferrer">
+                  <img src={mail} alt="Telegram Icon" />
+                  <span>hyolanda.h@gmail</span>
+                </a>
+              </ContactAddress>
+            </div>
+            <div className="col-lg-4 text-center">
+              <ContactAddress>
+                <a
+                  href="https://www.linkedin.com/in/hyolanda-fava/"
+                  title="Contact me via LinkedIn"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={linkedin} alt="Telegram Icon" />
+                  <span>in/hyolanda-fava</span>
+                </a>
+              </ContactAddress>
+            </div>
           </div>
+          <Girl>
+            <img src={girl} alt="Me answering your messages" />
+          </Girl>
         </div>
       </View>
     </main>
