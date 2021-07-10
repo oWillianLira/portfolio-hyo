397a3f72fb959a910e79d86c240c8add8774ee04
Willian Lira <will.lirag3@gmail.com>
2021-07-09 22:25:42 -0300

    test - import image from local and using lightbox (animations page)

--- src/Views/Arts/Animations/index.js
+++ src/Views/Arts/Animations/index.js
@@ -2,19 +2,20 @@ import React from 'react';
 import styled from 'styled-components';
 import { colors } from '../../../GlobalStyles';
 import MenuCircles from '../../Components/MenuCircles';
+import SimpleReactLightbox, { SRLWrapper } from 'simple-react-lightbox';
 
 import cartaz1 from '../../../Arts/static/cartaz1.jpg';
 import mockup1 from '../../../Arts/static/cartaz1-mockup.png';
-import cartaz2 from '../../../Arts/static/cartaz2.jpg';
-import mockup2 from '../../../Arts/static/cartaz2-mockup.png';
-import cartaz3 from '../../../Arts/static/cartaz3.jpg';
-import mockup3 from '../../../Arts/static/cartaz3-mockup.png';
-import cartaz4 from '../../../Arts/static/cartaz4.jpg';
-import mockup4 from '../../../Arts/static/cartaz4-mockup.png';
-import cartaz5 from '../../../Arts/static/cartaz5.jpg';
-import mockup5 from '../../../Arts/static/cartaz5-mockup.png';
-import cartaz6 from '../../../Arts/static/cartaz6.jpg';
-import mockup6 from '../../../Arts/static/cartaz6-mockup.png';
+// import cartaz2 from '../../../Arts/static/cartaz2.jpg';
+// import mockup2 from '../../../Arts/static/cartaz2-mockup.png';
+// import cartaz3 from '../../../Arts/static/cartaz3.jpg';
+// import mockup3 from '../../../Arts/static/cartaz3-mockup.png';
+// import cartaz4 from '../../../Arts/static/cartaz4.jpg';
+// import mockup4 from '../../../Arts/static/cartaz4-mockup.png';
+// import cartaz5 from '../../../Arts/static/cartaz5.jpg';
+// import mockup5 from '../../../Arts/static/cartaz5-mockup.png';
+// import cartaz6 from '../../../Arts/static/cartaz6.jpg';
+// import mockup6 from '../../../Arts/static/cartaz6-mockup.png';
 
 export default function Animations() {
   const View = styled.section`
@@ -55,14 +56,19 @@ export default function Animations() {
             links={colors.brown1}
           />
           <br />
-          <img
+          {/* <img
             src="https://images.unsplash.com/photo-1625851439411-749ce911fbb6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=80"
             Style="width: 350px;"
             alt=""
-          />
-          <img src={cartaz1} Style="width: 350px" alt="" />
-          <img src={mockup1} Style="width: 350px" alt="" />
-          <img src={cartaz2} Style="width: 350px" alt="" />
+          /> */}
+          <SimpleReactLightbox>
+            <SRLWrapper>
+              <a href={cartaz1}>
+                <img src={mockup1} Style="width: 350px" alt="" />
+              </a>
+            </SRLWrapper>
+          </SimpleReactLightbox>
+          {/* <img src={cartaz2} Style="width: 350px" alt="" />
           <img src={mockup2} Style="width: 350px" alt="" />
           <img src={cartaz3} Style="width: 350px" alt="" />
           <img src={mockup3} Style="width: 350px" alt="" />
@@ -71,7 +77,7 @@ export default function Animations() {
           <img src={cartaz5} Style="width: 350px" alt="" />
           <img src={mockup5} Style="width: 350px" alt="" />
           <img src={cartaz6} Style="width: 350px" alt="" />
-          <img src={mockup6} Style="width: 350px" alt="" />
+          <img src={mockup6} Style="width: 350px" alt="" /> */}
         </div>
       </View>
     </main>
