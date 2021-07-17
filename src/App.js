1b1a9360b5bbaca12d9b8ffc9f2e98e67a5f0162
Willian Lira <will.lirag3@gmail.com>
2021-07-16 21:01:49 -0300

    GA

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './routes';
 import GlobalStyle from './GlobalStyles';
 
+// import RouteChangeTracker from './RouteChangeTracker';
+// import ReactGA from 'react-ga';
+// const TRACKING_ID = 'G-6HLR76GTP7';
+// ReactGA.initialize(TRACKING_ID);
+
 import ReactGA from 'react-ga';
-const TRACKING_ID = 'G-6HLR76GTP7';
-ReactGA.initialize(TRACKING_ID);
-import RouteChangeTracker from './RouteChangeTracker';
+ReactGA.initialize('G-6HLR76GTP7');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 function App() {
   return (
